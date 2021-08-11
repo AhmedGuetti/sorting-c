1d8c8af53485e1c3bf13b039eb6aef75b401a788
Ahmed Guetti <guetti4ahmed@gmail.com>
2021-08-11 14:34:19 +0100

    Bubble sorte algorithm

--- sorting.c
+++ sorting.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdbool.h>
 #include"sorting.h"
 
 void swap(int * arr,int position1,int position2){
@@ -9,16 +10,25 @@ void swap(int * arr,int position1,int position2){
 
 
 void selection_sorting(int* arr,int size){
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size+1; j++)
-        {
-            if (arr[i]<arr[j])
-            {
+    for (int i = size-1; i >= 1; i--){
+        for (int j = 0; j < i; j++){
+            if (arr[j]>arr[i]){
                 swap(arr,i,j);
+            }   
+        }   
+    }
+}
+
+void bubble_sorte(int* arr,int size){
+    for (int i = size-1; i >= 1; i--){
+        bool is_sorted = true;
+        for (int j = 1; j <= i; j++){
+            if (arr[j-1]>arr[j]){
+                swap(arr,j-1,j);
+                is_sorted = false;
             }
-        }
-        
+        } 
+        if (is_sorted) return;  
     }
     
 }
--- sorting.h
+++ sorting.h
@@ -2,5 +2,6 @@
 #define _SORTING_H_
 
 void selection_sorting(int* arr,int size);
+void bubble_sorte(int* arr,int size);
 void swap(int * arr,int position1,int position2);
 #endif
