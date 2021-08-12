f59538a0e55c7cf1b9c2e21db162d53b0a867a7d
Ahmed Guetti <guetti4ahmed@gmail.com>
2021-08-12 02:24:00 +0100

    fixed using the a new aproche reducing memory also

--- sorting.c
+++ sorting.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<stdbool.h>
 #include"sorting.h"
 
@@ -48,49 +49,40 @@ void insertion_sorting(int* arr,int size){
     
 }
 
-
+//<= and >= Very important 
 void merge(int* arr,int l,int m,int r){
-    int i,j,k;
-    int n1 = m-l+1; 
-    int n2 = r-m;
-    int L[n1],R[n2];
+    int n = r-l+1;
+    int* temp = (int*)malloc(n*sizeof(int));
+    int left = l,right = m+1,index=0;
+    while (left<=m && right<=r){
+        if (arr[left]<=arr[right]){
+            temp[index] = arr[left];
+            left++; 
+        }
+        else{
+            temp[index] = arr[right];
+            right++; 
+        }
+        index++;
+    }
+    while(left <= m) {
+        temp[index] = arr[left];
+            left++; 
+            index++;
 
+    } 
+    while(right <= r)  {
+        temp[index] = arr[right];
+            right++; 
+            index++;
+    }
 
-    for (i = 0; i < n1; i++)
-        L[i] =  arr[l+i];
-    for (j = 0; j < n2; j++)
-        R[j] =  arr[m+1+j];
-    i = 0;
-    j = 0;
-    k = l;
-
-   while(i<n1 && j<n2){
-       if (L[i]<R[j])
-       {
-           arr[k] = L[i];
-           i++;
-       }
-       else{
-           arr[k] = R[j];
-           j++;
-       }
-       k++;
-   } 
-
-
-
-while (i<n1){
-    arr[k] = L[i];
-    i++;
-    k++;
-}
-while (j<n2){
-    arr[k] = R[j];
-    j++;
-    k++;
-}
-
-
+    for (int i = 0; i < n; i++){
+        arr[l+i]=temp[i];
+    }
+    free(temp);
+    
+    
 }
 
 void merge_sorting(int* arr,int l,int r){
